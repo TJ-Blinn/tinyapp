823c08a7654b00eb21abf5fd7f345d6795006002
TJ-Blinn <tjblinn@hotmail.com>
2021-08-27 03:04:48 +0000

    Step 3 — Adding the EJS Partials to Views

--- express_server.js
+++ express_server.js
@@ -2,14 +2,37 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+// -------------------------------
+// use res.render to load up an ejs view file
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+// index page
+app.get("/", function (req, res) {
+  const mascots = [
+    { name: "Sammy", organization: "DigitalOcean", birth_year: 2012 },
+    { name: "Tux", organization: "Linux", birth_year: 1996 },
+    { name: "Moby Dock", organization: "Docker", birth_year: 2013 },
+  ];
+  let tagline = "No programming concept is complete without a cute animal mascot.";
+
+  res.render("pages/index", {
+    // res.render() will look in a views folder for the view.
+    mascots: mascots,
+    tagline: tagline,
+  });
 });
+
+// about page
+app.get("/about", function (req, res) {
+  res.render("pages/about");
+});
+// -------------------------------
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
